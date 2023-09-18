055eb83c2d499615d5ab4ce65dacf7f0b0a4e4bc
Andreas Borowczak <andreas.borowczak@googlemail.com>
2023-09-18 23:42:26 +0200

    neue Strucktur

--- logistik/views.py
+++ logistik/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, HttpResponse
-from pit.models import Server, Schiene
 from kurse.models import Kurs
-from kunden.models import Kunde
-from trainer.models import Trainer
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Logistik index.")
+    test = Kurs.objects.all()
+
+    context = {'test': test,
+               }
+    return render(request, 'logistik/test.html', context)
