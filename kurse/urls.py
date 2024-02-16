ca07991067262dfd79f3fa4fa01f9cf2c31e3f76
Andreas borowczak <a.borowczak@mlgruppe.de>
2024-02-16 12:15:56 +0100

    Kursliste erstellt und testweise farblich codiert

--- kurse/urls.py
+++ kurse/urls.py
@@ -6,5 +6,6 @@ app_name = 'kurs'
 urlpatterns = [
     # Andere URLs hier ...
     path('create/', views.create_kurs, name='create_kurs'),
+    path('kurs_uebersicht/', views.kurs_uebersicht, name='kurs_uebersicht'),
     # path('list/', views.kurs_list, name='kurs_list'),   Annahme, dass du auch eine kurs_list-View hast
 ]
--- kurse/views.py
+++ kurse/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Trainer
+from .models import Trainer, Kurs
 from .forms import KursForm
+from datetime import datetime, timedelta
+from collections import defaultdict
+
+
+def kurs_uebersicht(request):
+    kurse = Kurs.objects.all()
+
+    kurs_table = {}
+    for kurs in kurse:
+        if kurs.kurs_start:
+            kunde = str(kurs.kunde)
+            kw = f"{kurs.kurs_start.isocalendar()[1]}/{kurs.kurs_start.year}"
+            kurs_table.setdefault(kunde, {}).setdefault(kw, []).append(kurs)
+
+    calendar_weeks = [f"{week}/{datetime.now().year}" for week in range(1, 53)]
+
+    context = {
+        'kurs_table': kurs_table,
+        'calendar_weeks': calendar_weeks
+    }
+    return render(request, 'kurse/kurs_uebersicht.html', context)
 
 
 def is_orga_group(user):
